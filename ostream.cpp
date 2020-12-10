661fba66a7af7ebaa01c2665f2d724197b327557
codepolarisuser <62128198+codepolarisuser@users.noreply.github.com>
2020-12-10 15:14:48 +0530

    update

--- ostream.cpp
+++ ostream.cpp
@@ -22,6 +22,12 @@ ostream & operator << (ostream& out, const set<T>&ST)
          out << endl;
     return out; 
 }
+template<typename T>
+ostream& operator << (ostream &out, unordered_set<T>&ST)
+{
+    for (const auto &x : ST) out << x <<" ";
+        return out;
+}
 template<typename T,typename U>
 ostream &operator <<(ostream &out,const map<T,U>&MP)
 {
@@ -31,6 +37,6 @@ ostream &operator <<(ostream &out,const map<T,U>&MP)
 
 int main ()
 {
-    map<int, string>mp{{1,"stuti"},{2,"knwfe"}};
-    cout << mp;
+    unordered_set<string>OS{"stuti","anand","polaris"};
+    cout << OS;
 }
